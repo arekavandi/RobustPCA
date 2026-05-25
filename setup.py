98d616c4b9db7ab2afaa0bad3cad8856669fd10a
Aref Miri Rekavandi <101369948+arekavandi@users.noreply.github.com>
2026-05-25 21:44:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     long_description=open('README.md').read(),
     author = ['Shun Chi'],
     install_requires=[
-        "numpy","scipy","tqdm"
+        "numpy","scipy","tqdm","fbpca"
     ],
 )
